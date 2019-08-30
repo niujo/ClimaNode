95978370b2bb0c0bcbb07f69906180a0c549ca4b
niujo <cla.cardenasd@gmail.com>
2019-08-30 13:14:20 -0400

    3 comit no se de que

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const lugar = require('./lugar/lugar');
 const axios = require('axios');
 
 const argv = require('yargs').options({
@@ -10,16 +11,6 @@ const argv = require('yargs').options({
 
 console.log(argv.direccion);
 
-const encodeUrl = encodeURI(argv.direccion);
+lugar.getLugarLat(argv.direccion)
+    .then
 
-const instance = axios.create({
-    baseURL : ` https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
-    headers : {"x-rapidapi-key" : "6c8fe1661fmsh5816f81c1f71f12p1b4598jsncc028dd1278a"}
-});
-instance.get()
-        .then(resp=>{
-            console.log(resp.data.Results[0]);
-        })
-        .catch(err =>{
-            console.log("error",err);
-        });
---
+++ lugar/lugar.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const getLugarLat = async (dir)=>{
+    const encodeUrl = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL : ` https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+        headers : {"x-rapidapi-key" : "6c8fe1661fmsh5816f81c1f71f12p1b4598jsncc028dd1278a"}
+    });
+    
+    const resp = await instance.get();
+
+    if (resp.data.Results.lenght === 0) {
+        throw new Error(`No hay resultados para ${direccion}`);
+    }
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+    module.exports = {
+        getLugarLat
+    }
+}   
+
